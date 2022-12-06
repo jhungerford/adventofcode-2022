96b3623f463a503a9526e4ca314e92a7b09950c8
John Hungerford <jhungerford@gmail.com>
2022-12-06 06:11:41 -0700

    Day 6 part 2.

--- src/day6.rs
+++ src/day6.rs
@@ -1,16 +1,20 @@
 use std::fs::read_to_string;
 use itertools::Itertools;
 
+static PACKET: usize = 4;
+static MESSAGE: usize = 14;
+
 pub fn solution() {
     let data = read_to_string("input/day6.txt").unwrap();
 
-    println!("Part 1: {}", first_marker(data.trim()));
+    println!("Part 1: {}", marker_idx(data.trim(), PACKET));
+    println!("Part 2: {}", marker_idx(data.trim(), MESSAGE));
 }
 
-fn first_marker(data: &str) -> usize {
-    4 + data.as_bytes()
-        .windows(4)
-        .position(|packet| packet.iter().unique().count() == 4)
+fn marker_idx(data: &str, len: usize) -> usize {
+    len + data.as_bytes()
+        .windows(len)
+        .position(|packet| packet.iter().unique().count() == len)
         .unwrap_or(0)
 }
 
@@ -19,11 +23,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_first_marker() {
-        assert_eq!(5, first_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"));
-        assert_eq!(6, first_marker("nppdvjthqldpwncqszvftbrmjlhg"));
-        assert_eq!(10, first_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
-        assert_eq!(11, first_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+    fn test_marker_idx_packet() {
+        assert_eq!(7, marker_idx("mjqjpqmgbljsphdztnvjfqwrcgsmlb", PACKET));
+        assert_eq!(5, marker_idx("bvwbjplbgvbhsrlpgdmjqwftvncz", PACKET));
+        assert_eq!(6, marker_idx("nppdvjthqldpwncqszvftbrmjlhg", PACKET));
+        assert_eq!(10, marker_idx("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", PACKET));
+        assert_eq!(11, marker_idx("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", PACKET));
+    }
+
+    #[test]
+    fn test_marker_idx_message() {
+        assert_eq!(19, marker_idx("mjqjpqmgbljsphdztnvjfqwrcgsmlb", MESSAGE));
+        assert_eq!(23, marker_idx("bvwbjplbgvbhsrlpgdmjqwftvncz", MESSAGE));
+        assert_eq!(23, marker_idx("nppdvjthqldpwncqszvftbrmjlhg", MESSAGE));
+        assert_eq!(29, marker_idx("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", MESSAGE));
+        assert_eq!(26, marker_idx("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", MESSAGE));
 
     }
 }
