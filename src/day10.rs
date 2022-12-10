b926830bcf874b63ae385a3c36d2787b2c5882ae
John Hungerford <jhungerford@gmail.com>
2022-12-10 12:57:53 -0700

    Day 10 part 2.

--- src/day10.rs
+++ src/day10.rs
@@ -9,6 +9,7 @@ pub fn solution() {
 
     println!("Day 10");
     println!("Part 1: {}", signal_strength(&instructions));
+    println!("Part 2:\n{}", render_image(&instructions));
 }
 
 struct Computer {
@@ -16,15 +17,23 @@ struct Computer {
 }
 
 impl Computer {
+    /// new returns a new computer.
     fn new() -> Self {
         Computer { register: 1 }
     }
 
+    /// run executes the given instruction.
     fn run(&mut self, instruction: &Instruction) {
         match instruction {
             Instruction::Noop => {},
             Instruction::AddX(value) => self.register += value,
         }
+    }
+
+    /// is_lit returns whether the given pixel is lit based on the computer's register.
+    /// The two pixels beside the register are lit, all other pixels are dark.
+    fn is_lit(&self, pixel: usize) -> bool {
+        (self.register-1..=self.register+1).contains(&(pixel as i32))
     }
 }
 
@@ -34,7 +43,6 @@ enum Instruction {
 }
 
 impl Instruction {
-
     /// cycles returns the number of cycles this instruction takes to complete.
     fn cycles(&self) -> i32 {
         match self {
@@ -66,8 +74,8 @@ fn parse_instructions(filename: &str) -> Vec<Instruction> {
 /// signal_strength returns the signal strength - cycle number multiplied by the register during
 /// the 20th cycle, and every 40 cycles after that
 fn signal_strength(instructions: &Vec<Instruction>) -> i32 {
-    let mut ss = 0;
     let mut comp = Computer::new();
+    let mut ss = 0;
     let mut i = 0;
 
     for instruction in instructions.iter() {
@@ -85,6 +93,33 @@ fn signal_strength(instructions: &Vec<Instruction>) -> i32 {
     ss
 }
 
+/// print_image runs the instructions, printing the image that results.
+fn render_image(instructions: &Vec<Instruction>) -> String {
+    let mut comp = Computer::new();
+    let mut str = String::new();
+    let mut i = 0;
+
+    for instruction in instructions.iter() {
+        for _ in 0..instruction.cycles() {
+            if comp.is_lit(i % 40) {
+                str = format!("{}#", str);
+            } else {
+                str = format!("{}.", str);
+            }
+
+            i += 1;
+
+            if (i % 40) == 0 {
+                str = format!("{}\n", str);
+            }
+        }
+
+        comp.run(instruction);
+    }
+
+    str.to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -94,5 +129,19 @@ mod tests {
         let instructions = parse_instructions("input/day10_sample.txt");
 
         assert_eq!(13140, signal_strength(&instructions));
+    }
+
+    #[test]
+    fn test_render_image() {
+        let instructions = parse_instructions("input/day10_sample.txt");
+        let expected = "\
+##..##..##..##..##..##..##..##..##..##..\n\
+###...###...###...###...###...###...###.\n\
+####....####....####....####....####....\n\
+#####.....#####.....#####.....#####.....\n\
+######......######......######......####\n\
+#######.......#######.......#######.....\n";
+
+        assert_eq!(expected, render_image(&instructions));
     }
 }
