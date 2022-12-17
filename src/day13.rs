2dc394e9b86a14eb34d93e9c57d00c51cf1a6a19
John Hungerford <jhungerford@gmail.com>
2022-12-17 08:06:55 -0700

    Day 13 part 1.

--- src/day13.rs
+++ src/day13.rs
@@ -1,95 +1,81 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::str::FromStr;
+use std::string::ParseError;
 use itertools::Itertools;
+use serde_json::Value;
+use serde_json::Value::{Array, Number};
 
 #[allow(dead_code)]
 pub fn solution() {
     let pairs = parse_pairs("input/day13.txt");
 
     println!("Day 13");
-    println!("Part 1: {}", count_ordered(&pairs)); // 6252 is too high, 6182 is too low.
+    println!("Part 1: {}", count_ordered(&pairs));
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum Node {
+    Array(Vec<Node>),
+    Number(i64),
+}
+
+impl FromStr for Node {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        fn parse(value: Value) -> Node {
+            match value {
+                Array(arr) => Node::Array(arr.into_iter().map(parse).collect_vec()),
+                Number(num) => Node::Number(num.as_i64().unwrap()),
+                _ => panic!("Unsupported value: {:?}", value),
+            }
+        }
+
+        // [[1],[2,3,4]]
+        let json = serde_json::from_str(s).unwrap();
+
+        Ok(parse(json))
+    }
 }
 
 struct Pair {
-    left: String,
-    right: String,
+    left: Node,
+    right: Node,
 }
 
 impl Pair {
     fn ordered(&self) -> bool {
-        let mut left = self.left.chars().peekable();
-        let mut right = self.right.chars().peekable();
-        let (mut left_nest, mut right_nest) = (0, 0);
-
-        loop {
-            match (left.peek(), right.peek()) {
-                // End of packet
-                (None, Some(_)) => return true,
-                (Some(_), None) => return false,
-
-                // List
-                (Some('['), Some('[')) => {
-                    left.next();
-                    right.next();
+        fn ordered_recursive(left: &Node, right: &Node) -> Option<bool> {
+            match (left, right) {
+                (Node::Array(l), Node::Array(r)) => {
+                    l.into_iter().zip(r.into_iter())
+                        .map(|(left_item, right_item)| ordered_recursive(left_item, right_item))
+                        .flatten()
+                        .nth(0)
+                        .or(if l.len() == r.len() {
+                            None
+                        } else {
+                            Some(l.len() < r.len())
+                        })
                 },
-                (Some(']'), Some(']')) => {
-                    if left_nest > 0 {
-                        left_nest -= 1;
-                    } else {
-                        left.next();
+                (Node::Number(l), Node::Array(_)) => {
+                    ordered_recursive(&Node::Array(vec![Node::Number(*l)]), right)
+                },
+                (Node::Array(_), Node::Number(r)) => {
+                    ordered_recursive(left, &Node::Array(vec![Node::Number(*r)]))
+                },
+                (Node::Number(l), Node::Number(r)) => {
+                    if l == r {
+                        return None
                     }
 
-                    if right_nest > 0 {
-                        right_nest -= 1;
-                    } else {
-                        right.next();
-                    }
-                },
-
-                // List - left ran out first
-                (Some(','), Some(',')) if left_nest > 0 => return true,
-                (Some(',') | Some(']'), Some(r)) if r.is_ascii_digit() => return true,
-                (Some(']'), Some(',') | Some('[')) => return true,
-
-                // List - right ran out first
-                (Some(','), Some(',')) if right_nest > 0 => return false,
-                (Some(l), Some(',') | Some(']')) if l.is_ascii_digit() => return false,
-                (Some(',') | Some('['), Some(']')) => return false,
-
-                // List - iterating
-                (Some(','), Some(',')) => {
-                    left.next();
-                    right.next();
-                },
-
-                // List - digits
-                (Some(l), Some(r)) if l.is_ascii_digit() && r.is_ascii_digit() => {
-                    let left_num = left.peeking_take_while(|c| c.is_ascii_digit())
-                        .collect::<String>().parse::<i32>().unwrap();
-                    let right_num = right.peeking_take_while(|c| c.is_ascii_digit())
-                        .collect::<String>().parse::<i32>().unwrap();
-
-                    if left_num < right_num {
-                        return true;
-                    } else if right_num < left_num {
-                        return false;
-                    }
-                },
-                (Some('['), Some(r)) if r.is_ascii_digit() => {
-                    left.next();
-                    right_nest += 1;
-                },
-                (Some(l), Some('[')) if l.is_ascii_digit() => {
-                    left_nest += 1;
-                    right.next();
-                },
-
-                // Unimplemented case
-                (l, r) => {
-                    unreachable!("'{}', '{}' - Unhandled case: {:?}, {:?}", self.left, self.right, l, r)
+                    Some(l < r)
                 },
             }
         }
+
+        ordered_recursive(&self.left, &self.right).unwrap()
     }
 }
 
@@ -103,19 +89,16 @@ fn parse_pairs(filename: &str) -> Vec<Pair> {
     // Pairs are two lines followed by a newline.
     loop {
         match (lines.next(), lines.next(), lines.next()) {
-            (Some(left), Some(right), _) => pairs.push(Pair { left, right }),
+            (Some(left), Some(right), _) => pairs.push(Pair {
+                left: left.parse().unwrap(),
+                right: right.parse().unwrap(),
+            }),
             _ => return pairs,
         }
     }
 }
 
 fn count_ordered(pairs: &Vec<Pair>) -> usize {
-    for pair in pairs {
-        if pair.ordered() {
-            println!("Ordered: {}\n\t{}\n\t{}", pair.ordered(), pair.left, pair.right);
-        }
-    }
-
     pairs.iter().enumerate()
         .filter(|(_, pair)| pair.ordered())
         .map(|(i, _)| i + 1)
@@ -134,12 +117,37 @@ mod tests {
     }
 
     #[test]
+    fn test_ordered_sample() {
+        let pairs = parse_pairs("input/day13_sample.txt");
+        let expected = vec![true, true, false, true, false, true, false, false];
+
+        for (i, (pair, exp)) in pairs.into_iter().zip(expected.into_iter()).enumerate() {
+            assert_eq!(exp, pair.ordered(), "Pair {}", i+1);
+        }
+    }
+
+    #[test]
+    fn test_parse() {
+        let node: Node = "[[[1,[],0]]]".parse().unwrap();
+
+        let expected = Node::Array(vec![
+            Node::Array(vec![
+                Node::Array(vec![
+                    Node::Number(1), Node::Array(vec![]), Node::Number(0),
+                ])
+            ])
+        ]);
+
+        assert_eq!(expected, node);
+    }
+
+    #[test]
     fn test_ordered() {
         let pair = Pair {
-            left: "[[[1,[],0]]]".to_string(),
-            right: "[[1,5,[9,5]]".to_string(),
+            left: "[[[1,[],0]]]".parse().unwrap(),
+            right: "[[1,5,[9,5]]]".parse().unwrap(),
         };
 
-        assert_eq!(false, pair.ordered(), "{}\n{}", pair.left, pair.right);
+        assert_eq!(false, pair.ordered());
     }
 }
